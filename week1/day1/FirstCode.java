a4d6140cc01a9214e6b964f5fa06600b48c3c1e0
Dillibabu2403 <118703394+Dillibabu2403@users.noreply.github.com>
2022-11-21 15:17:15 +0530

    Add files via upload
    Week1Day1Assignments

---
+++ week1/day1/FirstCode.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+public class FirstCode {
+ 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First Java exercise");
+		System.out.println("Dillibabu");
+
+	}
+
+}
---
+++ week1/day1/Mobile1.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Mobile1 {
+
+	public void makeCall() {
+		System.out.println("Calling someone");
+	}
+	
+	public void sendMsg() {
+		System.out.println("Chatting with friends");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Mobile1 obj=new Mobile1();
+		obj.makeCall();
+		obj.sendMsg();
+			
+		}
+
+	}
