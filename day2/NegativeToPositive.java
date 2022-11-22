966d384609ac510d9c3d439b24a79e3c02a6a0a8
Dillibabu2403 <118703394+Dillibabu2403@users.noreply.github.com>
2022-11-22 17:17:12 +0530

    Add files via upload
    Week1day2assignment

---
+++ day2/NegativeToPositive.java
@@ -0,0 +1,17 @@
+package week1.day2;
+
+public class NegativeToPositive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int value=-60;
+if(value<0) {
+	int num=value*-1;
+	System.out.println(num);
+}
+else {
+	System.out.println("Given number is positive");
+}
+	}
+
+}
