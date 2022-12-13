b44da660a6896f33b43aa881352260f0f6110a07
Dillibabu2403 <118703394+Dillibabu2403@users.noreply.github.com>
2022-12-13 16:29:20 +0530

    Add files via upload
    baseclassupdated

---
+++ week4day2/BaseClass.java
@@ -0,0 +1,60 @@
+package week4.day2;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseClass {
+
+	public RemoteWebDriver driver;
+
+	@Parameters({ "browser","url", "username", "password" })
+
+	@BeforeMethod
+	public void preConditions(String browser,String url, String username, String password) {
+		
+		if(browser.equalsIgnoreCase("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			ChromeOptions ch=new ChromeOptions();
+			ch.addArguments("--disable-notifications");
+			driver=new ChromeDriver(ch);
+		}
+		else if(browser.equalsIgnoreCase("edge")) {
+			WebDriverManager.edgedriver().setup();
+			EdgeOptions eh=new EdgeOptions();
+			eh.addArguments("--disable-notifications");
+			driver=new EdgeDriver(eh);
+		}
+		
+		else if(browser.equalsIgnoreCase("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			FirefoxOptions fh=new FirefoxOptions();
+			fh.addArguments("--disable-notifications");
+			driver=new FirefoxDriver(fh);
+		}
+		
+		driver.get(url);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.findElement(By.xpath("//input[@id='username']")).sendKeys(username);
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys(password);
+		driver.findElement(By.xpath("//input[@id='Login']")).click();
+	}
+
+	@AfterMethod
+	public void postConditions() {
+		driver.close();
+	}
+}
