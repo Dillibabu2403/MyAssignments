23567fa69f70892b4087a13f682d69cad56edb1e
Dillibabu2403 <118703394+Dillibabu2403@users.noreply.github.com>
2022-12-17 00:54:56 +0530

    Add files via upload
    week4day4assignment

---
+++ week4day4/CreateOpportunityWithoutMandatoryFields.java
@@ -0,0 +1,48 @@
+package week4.day4;
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class CreateOpportunityWithoutMandatoryFields extends BaseClassSalesForce {
+	
+	@DataProvider(name="CreateWithoutMandatory")
+	public String[][] editLeads() throws IOException {
+		String [] [] data=ReadExcelForCreateOppWithoutMandatory.read();
+		return data;
+	}
+	
+	@Test(dataProvider="CreateWithoutMandatory")
+	public void runOpportunityWithoutMandatoryFields(String VerifyAlert,String descName) throws InterruptedException {
+		driver.findElement(By.xpath("//div[@class='slds-icon-waffle']")).click();
+		driver.findElement(By.xpath("//button[text()='View All']")).click();
+		Thread.sleep(3000);
+		WebElement ele1 = driver.findElement(By.xpath("//p[text()='Sales']"));
+		driver.executeScript("arguments[0].click();", ele1);
+		Thread.sleep(5000);
+		WebElement ele2 = driver.findElement(By.xpath("//span[text()='Opportunities']"));
+		driver.executeScript("arguments[0].click();", ele2);
+		Thread.sleep(3000);
+		WebElement ele3 = driver.findElement(By.xpath("//div[text()='New']"));
+		driver.executeScript("arguments[0].click();", ele3);
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//input[@name='CloseDate']")).click();
+		Thread.sleep(3000);
+		WebElement ele4 = driver.findElement(By.xpath("//td[@class='slds-is-today']/following-sibling::td/span"));
+		driver.executeScript("arguments[0].click();", ele4);
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//textarea[@class='slds-textarea']")).sendKeys(descName);
+		driver.findElement(By.xpath("//button[text()='Save']")).click();
+		String text2 = driver.findElement(By.xpath("//div[@class='fieldLevelErrors']")).getText();
+		System.out.println(text2);
+		if (text2.contains(VerifyAlert)) {
+			System.out.println("Alert message matched");
+		} else {
+			System.out.println("Alert message not matched");
+		}
+		
+}
+}
---
+++ week4day4/ReadExcelForCreateNewContact.java
@@ -0,0 +1,34 @@
+package week4.day4;
+
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.Test;
+
+public class ReadExcelForCreateNewContact {
+
+public static String[][] read() throws IOException {
+	XSSFWorkbook wb=new XSSFWorkbook("./Data/CreateNewContact.xlsx");
+	XSSFSheet sheet=wb.getSheetAt(0);
+	int rowCount = sheet.getLastRowNum();
+	System.out.println(rowCount);
+	int cellNum = sheet.getRow(0).getLastCellNum();
+	System.out.println(cellNum);
+	String[][]data=new String[rowCount][cellNum];
+	for(int i=1;i<=rowCount;i++) {
+		XSSFRow firstData1 = sheet.getRow(i);
+		for(int j=0;j<cellNum;j++) {
+XSSFCell cell2 = firstData1.getCell(j);
+System.out.println(cell2.getStringCellValue());
+data[i-1][j]=cell2.getStringCellValue();
+	}
+	}
+	wb.close();
+	return data;
+}
+}
+	
+
